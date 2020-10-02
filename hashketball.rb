55a9151ad1011c1f21641553735334e8bf9f3c6a
Demetrio Lima <dlima.nyit@gmail.com>
2020-10-02 16:56:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -112,7 +112,15 @@
            blocks: 5,
            slam_dunks: 12
          }, {
-           
+           player_name: "Kemba Walker",
+           number: 33,
+           shoe: 15,
+           points: 6,
+           rebounds: 12,
+           assists: 12,
+           steals: 22,
+           blocks: 5,
+           slam_dunks: 12
          }
        ]
      }
